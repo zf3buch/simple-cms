175204e9b5c16af4e54eaac5bd66643ef8e9f083
Ralf Eggert <ralf@travello.com>
2016-08-04 14:35:56 +0200

    Added repo

---
+++ data/category-data.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * ZF3 book Simple CMS Training Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/simple-cms
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ *
+ */
+
+return [
+    '1' => [
+        'id'          => '1',
+        'updated'     => '2016-08-03 10:40:25',
+        'status'      => 'approved',
+        'name'        => 'Autos',
+        'url'         => 'autos',
+        'description' => 'Alles über schnelle Autos',
+        'image'       => '/categories/autos.jpg',
+    ],
+    '2' => [
+        'id'          => '2',
+        'updated'     => '2016-08-03 10:43:12',
+        'status'      => 'approved',
+        'name'        => 'Business',
+        'url'         => 'business',
+        'description' => 'Die neuesten Trends aus der Geschäftswelt',
+        'image'       => '/categories/business.jpg',
+    ],
+    '3' => [
+        'id'          => '3',
+        'updated'     => '2016-08-03 10:43:31',
+        'status'      => 'approved',
+        'name'        => 'Hunde',
+        'url'         => 'hunde',
+        'description' => 'Der Hund - des Menschen bester Freund',
+        'image'       => '/categories/hunde.jpg',
+    ],
+    '4' => [
+        'id'          => '4',
+        'updated'     => '2016-08-03 10:44:59',
+        'status'      => 'approved',
+        'name'        => 'Katzen',
+        'url'         => 'katzen',
+        'description' => 'Katzen gehen immer - und sind echt lieb',
+        'image'       => '/categories/katzen.jpg',
+    ],
+    '5' => [
+        'id'          => '5',
+        'updated'     => '2016-08-03 10:46:41',
+        'status'      => 'approved',
+        'name'        => 'Mode',
+        'url'         => 'mode',
+        'description' => 'Alles über Mode und Gedöns',
+        'image'       => '/categories/mode.jpg',
+    ],
+    '6' => [
+        'id'          => '6',
+        'updated'     => '2016-08-03 10:48:55',
+        'status'      => 'approved',
+        'name'        => 'Nachtleben',
+        'url'         => 'nachtleben',
+        'description' => 'Tipps für das Nachtleben',
+        'image'       => '/categories/nachtleben.jpg',
+    ],
+    '7' => [
+        'id'          => '7',
+        'updated'     => '2016-08-03 10:50:37',
+        'status'      => 'approved',
+        'name'        => 'Natur',
+        'url'         => 'natur',
+        'description' => 'Raus in die Natur mit unseren Tipps',
+        'image'       => '/categories/natur.jpg',
+    ],
+    '8' => [
+        'id'          => '8',
+        'updated'     => '2016-08-03 10:53:18',
+        'status'      => 'approved',
+        'name'        => 'Pizzen',
+        'url'         => 'pizzen',
+        'description' => 'Was gibt es besseres als Pizza?',
+        'image'       => '/categories/pizzen.jpg',
+    ],
+    '9' => [
+        'id'         => '9',
+        'updated'    => '2016-08-03 10:56:43',
+        'status'     => 'approved',
+        'name'        => 'Sport',
+        'url'         => 'sport',
+        'description' => 'Alles über Sport und Bewegung',
+        'image'       => '/categories/sport.jpg',
+    ],
+    '10' => [
+        'id'         => '10',
+        'updated'    => '2016-08-03 10:56:43',
+        'status'     => 'approved',
+        'name'        => 'Städte',
+        'url'         => 'staedte',
+        'description' => 'Tipps für die tollsten Städte',
+        'image'       => '/categories/staedte.jpg',
+    ],
+    '11' => [
+        'id'         => '11',
+        'updated'    => '2016-08-03 10:56:43',
+        'status'     => 'approved',
+        'name'        => 'Technik',
+        'url'         => 'technik',
+        'description' => 'Techniktrends und heiße Gadgets',
+        'image'       => '/categories/technik.jpg',
+    ],
+];
---
+++ module/PageModel/src/Repository/PageRepositoryFactory.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * ZF3 book Simple CMS Training Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/simple-cms
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ *
+ */
+
+namespace PageModel\Repository;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+/**
+ * Class PageRepositoryFactory
+ *
+ * @package PageModel\Repository
+ */
+class PageRepositoryFactory implements FactoryInterface
+{
+    public function __invoke(
+        ContainerInterface $container,
+        $requestedName,
+        array $options = null
+    ) {
+        $pageData     = include PROJECT_ROOT . '/data/page-data.php';
+        $categoryData = include PROJECT_ROOT . '/data/category-data.php';
+
+        return new PageRepository($pageData, $categoryData);
+    }
+}
